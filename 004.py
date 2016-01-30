e3c00116d0c5efd1b8403258ff4bf1ed5dd356a7
Cerilla <weiss.cerilla@gmail.com>
2016-01-30 14:01:22 +0700

    Completed problem 004

---
+++ 004.py
@@ -0,0 +1,22 @@
+# Problem No. 004
+# BIGGEST PALINDROME
+
+
+def is_palindrome(x):
+    str_pal = str(x)
+    for i in range(int(len(str_pal) / 2)):
+        if str_pal[i] != str_pal[-(i + 1)]:
+            return False
+    return True
+
+
+i = 100
+bigpal = 0
+
+for a in range(i, 1000):
+    for b in range(i, 1000):
+        val = a * b
+        if is_palindrome(val) and val > bigpal:
+            bigpal = val
+
+print(bigpal)
