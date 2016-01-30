3bfdfa0ffefc417f70ea1f0058d04911a8169a0f
Cerilla <weiss.cerilla@gmail.com>
2016-01-30 19:18:57 +0700

    Completed problem 006

---
+++ 006.py
@@ -0,0 +1,13 @@
+# Problem 006
+# Sum square difference
+
+n = 100
+square_sum = 0
+sum_square = 0
+
+for i in range(n+1):
+    sum_square += i**2
+    square_sum += i
+
+square_sum = square_sum ** 2
+print(square_sum - sum_square)
